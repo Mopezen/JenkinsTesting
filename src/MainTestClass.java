afe47cfbc1067eae3cad63fe631ed6b67de7e070
Jones <mejones@deloitte.ca>
2016-05-09 16:31:20 -0400

    First commit

---
+++ src/MainTestClass.java
@@ -0,0 +1,11 @@
+
+public class MainTestClass {
+	public static void main(String [] args){
+		System.out.println("Hello World!");
+		String message = "Yep I still got it!";
+		
+		for(int i = 0; i < 100; i++){
+			System.out.println(message);
+		}
+	}
+}
