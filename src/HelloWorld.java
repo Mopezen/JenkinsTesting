4f2b4592ba60ccc21cf1f175592fca9a609be945
Mopezen <MeshachJ@hotmail.com>
2016-05-09 16:50:19 -0400

    Renamed files

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,7 @@
 
-public class MainTestClass {
+public class HelloWorld {
 	public static void main(String [] args){
-		System.out.println("Hello World!");
-		String message = "Yep I still got it!";
+		String message = "Hello World!";
 		
 		for(int i = 0; i < 100; i++){
 			System.out.println(message);
